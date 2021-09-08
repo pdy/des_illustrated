b419c9595510ef65a282a32e012c4cda1054ffe0
Pawel Drzycimski <pdrzycimski@gmail.com>
2021-09-08 21:31:30 +0200

    Extend example program

--- test_data/des_create_example_data.c
+++ test_data/des_create_example_data.c
@@ -29,31 +29,71 @@
 
 int main(void)
 {
-  const char *data_filename = "data_single_block.bin";
-  const char *key_filename = "hex_key.txt";
-  const char *key = "133457799BBCDFF1";
-  const unsigned char data[] = { 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef };
-
-  FILE *data_file = fopen(data_filename, "wb");
-  if(!data_file)
+  const char *data_filename[] =
   {
-    printf("%s: %s\n", "Can't open the data file", data_filename);
-    return 0;
-  }
+    "data_1.bin"
+  };
 
-  FILE *key_file = fopen(key_filename, "w");
-  if(!key_file)
+  const char *key_filename[] =
   {
-    printf("%s: %s\n", "Can't open the key file", key_filename);
+    "hex_key_1.txt"
+  };
+
+  const char *cipher_filename[] = 
+  {
+    "cipher_1.bin"
+  };
+
+  const char *key[] =
+  {
+    "133457799BBCDFF1"
+  };
+  
+  const unsigned char data[1][8] = 
+  { 
+    { 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef }
+  };
+
+  const unsigned char cipher[1][8] = 
+  { 
+    { 0x85, 0xe8, 0x13, 0x54, 0x0f, 0x0a, 0xb4, 0x05 }
+  };
+
+
+  for(size_t i = 0; i < 1; ++i)
+  {
+    FILE *data_file = fopen(data_filename[i], "wb");
+    if(!data_file)
+    {
+      printf("%s: %s\n", "Can't open the data file", data_filename[i]);
+      return 0;
+    }
+
+    FILE *key_file = fopen(key_filename[i], "w");
+    if(!key_file)
+    {
+      printf("%s: %s\n", "Can't open the key file", key_filename[i]);
+      fclose(data_file);
+      return 0;
+    }
+
+    FILE *cipher_file = fopen(cipher_filename[i], "w");
+    if(!cipher_file)
+    {
+      printf("%s: %s\n", "Can't open the cipher file", cipher_filename[i]);
+      fclose(data_file);
+      fclose(key_file);
+      return 0;
+    }
+
+    fwrite(data[i], 1, sizeof(data[i]), data_file);
+    fwrite(cipher[i], 1, sizeof(cipher[i]), cipher_file);
+    fprintf(key_file, "%s\n", key[i]);
+
     fclose(data_file);
-    return 0;
+    fclose(key_file);
+    fclose(cipher_file);
   }
-
-  fwrite(data, 1, sizeof(data), data_file);
-  fprintf(key_file, "%s\n", key);
-
-  fclose(data_file);
-  fclose(key_file);
    
   return 0;
 }
